4ca8e70d6af2dd41df66b6d3ac9c081c4f87418d
Lucas Previato <lucaspreviato@Live.com>
2023-03-02 22:33:08 -0300

    feat(core): add module prisma and graphql connection

--- apps/core/src/app.module.ts
+++ apps/core/src/app.module.ts
@@ -1,18 +1,35 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
-import { CompaniesModule } from './companies/companies.module';
-import { JobCategoriesModule } from './job-categories/job-categories.module';
-import { JobTitlesModule } from './job-titles/job-titles.module';
-import { JobtitlesModule } from './jobtitles/jobtitles.module';
-import { EmployeesModule } from './employees/employees.module';
-import { DepartmentsModule } from './departments/departments.module';
+import { GraphQLModule } from '@nestjs/graphql';
+import { join } from 'path';
+import { ApolloDriver, ApolloDriverConfig } from '@nestjs/apollo';
+import { ApolloServerPluginLandingPageLocalDefault } from 'apollo-server-core';
 import { LaboratoriesModule } from './laboratories/laboratories.module';
 import { UnitsModule } from './units/units.module';
-import { CompaniesModule } from './companies/companies.module';
+import { DepartmentsModule } from './departments/departments.module';
+import { EmployeesModule } from './employees/employees.module';
+import { JobCategoriesModule } from './job-categories/job-categories.module';
+import { JobTitlesModule } from './job-titles/job-titles.module';
+import { PrismaModule } from './prisma/prisma.module'
 
 @Module({
-  imports: [CompaniesModule, UnitsModule, LaboratoriesModule, DepartmentsModule, EmployeesModule, JobtitlesModule, JobTitlesModule, JobCategoriesModule],
+  imports: [
+    GraphQLModule.forRoot<ApolloDriverConfig>({
+      driver: ApolloDriver,
+      playground: false,
+      plugins: [ApolloServerPluginLandingPageLocalDefault()],
+      autoSchemaFile: join(process.cwd(), 'src/graphql/schema.gql'),
+      sortSchema: true,
+    }),
+    LaboratoriesModule,
+    UnitsModule,
+    DepartmentsModule,
+    EmployeesModule,
+    JobTitlesModule,
+    JobCategoriesModule,
+    PrismaModule,
+  ],
   controllers: [AppController],
   providers: [AppService],
 })
